e9753305a014d5b4ad17490ffddb23d730d2542d
ThomasOM <55256088+ThomasOM@users.noreply.github.com>
2022-11-10 17:08:53 -0600

    Remove data annotation from packet frames causing possible overlap conflicts

--- src/main/java/dev/thomazz/pledge/api/PacketFrame.java
+++ src/main/java/dev/thomazz/pledge/api/PacketFrame.java
@@ -1,13 +1,15 @@
 package dev.thomazz.pledge.api;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 /**
  * Represents a pair of signaling packets that wraps all packets sent during a server tick.
  */
-@Data
+@Getter
 @ToString
+@RequiredArgsConstructor
 public class PacketFrame {
     private final int id1;
     private final int id2;
