3dcfd8f95a9683676a976e8a43b8172a94814dec
ThomasOM <55256088+ThomasOM@users.noreply.github.com>
2024-03-22 15:18:39 -0600

    Release version 3.0

--- src/main/java/dev/thomazz/pledge/pinger/data/Ping.java
+++
@@ -1,9 +0,0 @@
-package dev.thomazz.pledge.pinger.data;
-
-import lombok.Data;
-
-@Data
-public class Ping {
-    private final PingOrder order;
-    private final int id;
-}
--- src/main/java/dev/thomazz/pledge/pinger/data/PingOrder.java
+++
@@ -1,6 +0,0 @@
-package dev.thomazz.pledge.pinger.data;
-
-public enum PingOrder {
-    TICK_START,
-    TICK_END
-}
